7004218acdc420a03ec7da5158ddcc873c856a47
Prime47457 <43023565+Prime47457@users.noreply.github.com>
2021-03-19 21:30:29 +0700

    Revert "add login page and sign up page"

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,6 @@
 import "../styles/globals.css";
 import "../styles/App.css";
-import '../styles/Login.css';
-import '../styles/SignUp.css';
-import { AppProps } from "next/app";
 import "fontsource-roboto";
-
 
 function App({ Component, pageProps }) {
   return <Component {...pageProps} />;
