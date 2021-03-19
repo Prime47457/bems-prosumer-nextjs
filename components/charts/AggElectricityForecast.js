0515d530aba33e003db919c588fffaa1761e07ca
Prime47457 <43023565+Prime47457@users.noreply.github.com>
2021-03-19 22:10:03 +0700

    fix(prime):add firebase authentication (#10)
    to fix git flow

---
+++ components/charts/AggElectricityForecast.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export function AggLoadForecast() {
+  return <h1>Aggregated Load and Forecast</h1>;
+}
+
+export function AggGenForecast() {
+  return <h1>Aggregated Generation and Forecast</h1>;
+}
--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,9 @@
 import "../styles/globals.css";
-import "../styles/Widget.css";
 import "../styles/App.css";
 import '../styles/Login.css';
 import '../styles/SignUp.css';
 import { AppProps } from "next/app";
+import "fontsource-roboto";
 
 
 function App({ Component, pageProps }) {
