ca491923987b332152b59d3e23ba60655f221107
Prime <primepianpucktr@gmail.com>
2021-04-19 22:40:51 +0700

    test(prime): test deploy

--- components/charts/Electricity.js
+++ components/charts/Electricity.js
@@ -1,7 +1,38 @@
 import React, { useState } from "react";
 import Chart from "react-apexcharts";
+import updateChart from "../../assets/updateChart";
+import firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: process.env.NEXT_APP_FIREBASE_KEY,
+  authDomain: process.env.NEXT_APP_FIREBASE_DOMAIN,
+  projectId: process.env.NEXT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_APP_FIREBASE_SENDER_ID,
+  appId: "1:1083389397572:web:1470b861f0c2209e9b8a11",
+  measurementId: "G-JQXCS7B1JK",
+};
+
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+}
 
 const Electricity = () => {
+  const user = firebase.auth().currentUser;
+  if (user !== null) {
+    firebase
+      .firestore()
+      .collection("users")
+      .doc(user.uid)
+      .get()
+      .then((doc) => {
+        const url = doc.data().link;
+        updateChart(url);
+        console.log("damn");
+      });
+  }
   const [chart, setChart] = useState({
     series: [],
     responsive: [
--- pages/prosumers/Prosumer.js
+++ pages/prosumers/Prosumer.js
@@ -6,7 +6,6 @@ import { MuiPickersUtilsProvider, DatePicker } from "@material-ui/pickers";
 import Navbar from "../../components/Navbar";
 import dynamic from "next/dynamic";
 import historicalData from "../../assets/historicalData";
-import updateChart from "../../assets/updateChart";
 
 const Electricity = dynamic(
   () => {
@@ -22,41 +21,9 @@ const Market = dynamic(
   { ssr: false }
 );
 
-import firebase from "firebase/app";
-import "firebase/auth";
-import "firebase/firestore";
-
-const firebaseConfig = {
-  apiKey: process.env.NEXT_APP_FIREBASE_KEY,
-  authDomain: process.env.NEXT_APP_FIREBASE_DOMAIN,
-  projectId: process.env.NEXT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_APP_FIREBASE_SENDER_ID,
-  appId: "1:1083389397572:web:1470b861f0c2209e9b8a11",
-  measurementId: "G-JQXCS7B1JK",
-};
-
-if (!firebase.apps.length) {
-  firebase.initializeApp(firebaseConfig);
-}
-
 export default function Prosumer() {
   const [selectedDate, setSelectedDate] = useState(new Date());
   const [selectedPriceDate, setSelectedPriceDate] = useState(new Date());
-
-  const user = firebase.auth().currentUser;
-  if (user === null) {
-    firebase
-      .firestore()
-      .collection("users")
-      .doc(user.uid)
-      .get()
-      .then((doc) => {
-        const url = doc.data().link;
-        updateChart(url);
-        console.log("damn");
-      });
-  }
 
   const handleDateChange = (date) => {
     setSelectedDate(date);
